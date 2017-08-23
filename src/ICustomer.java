1c1218151da49dab16c3743d58ef9748d6f2ac98
wassupjak <wassj93@gmail.com>
2017-08-23 16:26:08 +0100

    Add customer

---
+++ src/ICustomer.java
@@ -0,0 +1,56 @@
+
+public interface ICustomer {
+
+	Integer getCustomerId();
+
+	void setCustomerId(Integer customerId);
+
+	String getTitle();
+
+	void setTitle(String title);
+
+	String getFirstName();
+
+	void setFirstName(String firstName);
+
+	String getLastName();
+
+	void setLastName(String lastName);
+
+	String getAddress1();
+
+	void setAddress1(String address1);
+
+	String getAddress2();
+
+	void setAddress2(String address2);
+
+	String getCity();
+
+	void setCity(String city);
+
+	String getCounty();
+
+	void setCounty(String county);
+
+	String getPostCode();
+
+	void setPostCode(String postCode);
+
+	String getCountry();
+
+	void setCountry(String country);
+
+	String getHomePhone();
+
+	void setHomePhone(String homePhone);
+
+	String getMobilePhone();
+
+	void setMobilePhone(String mobilePhone);
+
+	String getEmail();
+
+	void setEmail(String email);
+
+}
